ca27890be529fbf9626544bfaeb087e8f4313ff3
Andrew Kane <andrew@ankane.org>
2024-11-23 00:24:31 -0800

    Added tests for merge metrics [skip ci]

--- test/merge_test.rb
+++ test/merge_test.rb
@@ -6,9 +6,13 @@ class MergeTest < Minitest::Test
     with_table(df) do |dt|
       source = Polars::DataFrame.new({"x" => [2, 3], "y" => [5, 8]})
 
-      dt.merge(source, "target.x = source.x", source_alias: "source", target_alias: "target")
-        .when_matched_update({"x" => "source.x", "y" => "source.y"})
-        .execute
+      metrics =
+        dt.merge(source, "target.x = source.x", source_alias: "source", target_alias: "target")
+          .when_matched_update({"x" => "source.x", "y" => "source.y"})
+          .execute
+      assert_equal 3, metrics["num_output_rows"]
+      assert_equal 1, metrics["num_target_files_added"]
+      assert_equal 1, metrics["num_target_files_removed"]
 
       expected = Polars::DataFrame.new({"x" => [1, 2, 3], "y" => [4, 5, 8]})
       assert_equal expected, dt.to_polars
@@ -20,9 +24,13 @@ class MergeTest < Minitest::Test
     with_table(df) do |dt|
       source = Polars::DataFrame.new({"x" => [2, 3, 7], "y" => [4, 5, 8]})
 
-      dt.merge(source, "target.x = source.x", source_alias: "source", target_alias: "target")
-        .when_not_matched_insert({"x" => "source.x", "y" => "source.y"})
-        .execute
+      metrics =
+        dt.merge(source, "target.x = source.x", source_alias: "source", target_alias: "target")
+          .when_not_matched_insert({"x" => "source.x", "y" => "source.y"})
+          .execute
+      assert_equal 1, metrics["num_output_rows"]
+      assert_equal 1, metrics["num_target_files_added"]
+      assert_equal 0, metrics["num_target_files_removed"]
 
       expected = Polars::DataFrame.new({"x" => [1, 2, 3, 7], "y" => [4, 5, 6, 8]})
       assert_equal expected, dt.to_polars
@@ -34,9 +42,13 @@ class MergeTest < Minitest::Test
     with_table(df) do |dt|
       source = Polars::DataFrame.new({"x" => [2, 3], "deleted" => [false, true]})
 
-      dt.merge(source, "target.x = source.x", source_alias: "source", target_alias: "target")
-        .when_matched_delete(predicate: "source.deleted = true")
-        .execute
+      metrics =
+        dt.merge(source, "target.x = source.x", source_alias: "source", target_alias: "target")
+          .when_matched_delete(predicate: "source.deleted = true")
+          .execute
+      assert_equal 2, metrics["num_output_rows"]
+      assert_equal 1, metrics["num_target_files_added"]
+      assert_equal 1, metrics["num_target_files_removed"]
 
       expected = Polars::DataFrame.new({"x" => [1, 2], "y" => [4, 5]})
       assert_equal expected, dt.to_polars
