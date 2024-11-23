65eca98660189ce7004c63a977f2f5186c478ffa
Andrew Kane <andrew@ankane.org>
2024-11-22 19:34:32 -0800

    Fixed CI

--- test/write_test.rb
+++ test/write_test.rb
@@ -7,8 +7,8 @@ class WriteTest < Minitest::Test
       DeltaLake.write(table_uri, df)
 
       dt = DeltaLake::Table.new(table_uri)
-      assert_equal df, dt.to_polars
       assert_equal 0, dt.version
+      assert_equal df, dt.to_polars
 
       error = assert_raises(DeltaLake::Error) do
         DeltaLake.write(dt, df)
@@ -16,35 +16,33 @@ class WriteTest < Minitest::Test
       assert_match "table already exists", error.message
 
       DeltaLake.write(dt, df, mode: "overwrite")
-      assert_equal df, dt.to_polars
       assert_equal 1, dt.version
+      assert_equal df, dt.to_polars
 
       time = Time.now
-      # version timestamps are stored at ms precision
-      # extra ms needed for CI
-      sleep(0.002)
+      sleep(0.01)
 
       DeltaLake.write(dt, df, mode: "ignore")
-      assert_equal df, dt.to_polars
       assert_equal 1, dt.version
+      assert_equal df, dt.to_polars
 
       DeltaLake.write(dt, df, mode: "append")
-      assert_equal Polars.concat([df, df]), dt.to_polars
       assert_equal 2, dt.version
+      assert_equal Polars.concat([df, df]), dt.to_polars
 
       assert_empty dt.transaction_versions
 
       dt.load_as_version(dt.version - 1)
-      assert_equal df, dt.to_polars
       assert_equal 1, dt.version
+      assert_equal df, dt.to_polars
 
       dt.load_as_version(time)
-      assert_equal df, dt.to_polars
       assert_equal 1, dt.version
+      assert_equal df, dt.to_polars
 
       dt = DeltaLake::Table.new(table_uri, version: 1)
-      assert_equal df, dt.to_polars
       assert_equal 1, dt.version
+      assert_equal df, dt.to_polars
     end
   end
 
