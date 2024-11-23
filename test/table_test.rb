33723d61e03d70bf0829636338c13f563f6e4c86
Andrew Kane <andrew@ankane.org>
2024-11-22 17:12:54 -0800

    Improved compact test

--- test/table_test.rb
+++ test/table_test.rb
@@ -98,23 +98,27 @@ class TableTest < Minitest::Test
   def test_compact
     df = Polars::DataFrame.new({"a" => [1, 2, 3]})
     with_table(df) do |dt|
+      2.times do
+        DeltaLake.write(dt, df, mode: "append")
+      end
+
       metrics = dt.optimize.compact
-      assert_equal 0, metrics["numFilesAdded"]
-      assert_equal 0, metrics["numFilesRemoved"]
-      assert_equal 0, metrics["partitionsOptimized"]
-      assert_equal 0, metrics["filesAdded"]["avg"]
-      assert_equal 0, metrics["filesAdded"]["max"]
-      assert_equal 0, metrics["filesAdded"]["min"]
-      assert_equal 0, metrics["filesAdded"]["totalFiles"]
-      assert_equal 0, metrics["filesAdded"]["totalSize"]
-      assert_equal 0, metrics["filesRemoved"]["avg"]
-      assert_equal 0, metrics["filesRemoved"]["max"]
-      assert_equal 0, metrics["filesRemoved"]["min"]
-      assert_equal 0, metrics["filesRemoved"]["totalFiles"]
-      assert_equal 0, metrics["filesRemoved"]["totalSize"]
-      assert_equal 0, metrics["numBatches"]
-      assert_equal 1, metrics["totalConsideredFiles"]
-      assert_equal 1, metrics["totalFilesSkipped"]
+      assert_equal 1, metrics["numFilesAdded"]
+      assert_equal 3, metrics["numFilesRemoved"]
+      assert_in_delta 584, metrics["filesAdded"]["avg"]
+      assert_equal 584, metrics["filesAdded"]["max"]
+      assert_equal 584, metrics["filesAdded"]["min"]
+      assert_equal 1, metrics["filesAdded"]["totalFiles"]
+      assert_equal 584, metrics["filesAdded"]["totalSize"]
+      assert_in_delta 571, metrics["filesRemoved"]["avg"]
+      assert_equal 571, metrics["filesRemoved"]["max"]
+      assert_equal 571, metrics["filesRemoved"]["min"]
+      assert_equal 3, metrics["filesRemoved"]["totalFiles"]
+      assert_equal 1713, metrics["filesRemoved"]["totalSize"]
+      assert_equal 1, metrics["partitionsOptimized"]
+      assert_equal 3, metrics["numBatches"]
+      assert_equal 3, metrics["totalConsideredFiles"]
+      assert_equal 0, metrics["totalFilesSkipped"]
       assert_equal true, metrics["preserveInsertionOrder"]
     end
   end
